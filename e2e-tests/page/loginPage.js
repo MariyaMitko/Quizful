8ec3e78a8236613b89ef2b28c8931f026bc13e34
Maryia Mitsko <maryia.mitsko@itechart-group.com>
2018-07-09 17:25:09 +0300

    dd

--- e2e-tests/page/loginPage.js
+++ e2e-tests/page/loginPage.js
@@ -15,8 +15,8 @@ let loginPage = function () {
         browser.wait(EC.presenceOf(element(by.name("loginForm.login"))), 15000,
             "field for entering of login did no appear in 15 sec")
 
-        await element(by.name("loginForm.login")).sendKeys(userLogin)
-        await element(by.name("loginForm.password")).sendKeys(userPassword)
+        await element(by.name('loginForm.login')).sendKeys(userLogin)
+        await element(by.name('loginForm.password')).sendKeys(userPassword)
         await element(by.name('ok')).click()
     }
 }
