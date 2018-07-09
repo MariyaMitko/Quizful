401522461b88300df68755369ddb9f54bad8ee3b
Maryia Mitsko <maryia.mitsko@itechart-group.com>
2018-07-09 16:01:25 +0300

    qqq

--- e2e-tests/test/logout_spec.js
+++ e2e-tests/test/logout_spec.js
@@ -4,9 +4,9 @@ describe('Third Quizful test', function () {
     const USER_LOGIN = "MariaMitko"
     const USER_PASSWORD = "ZcysqGthtw13"
 
-    let mainPage = require('../pages/mainPage.js')
-    let loginPage = require('../pages/loginPage.js')
-    let userPage = require('../pages/userPage.js')
+    let mainPage = require('../page/mainPage.js')
+    let loginPage = require('../page/loginPage.js')
+    let userPage = require('../page/userPage.js')
 
     beforeEach(async function () {
         browser.ignoreSynchronization = true
